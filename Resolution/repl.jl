f86428dd709864260f1dd391753ec02e5788385e
sazare <song.of.sand@gmail.com>
2018-03-31 08:33:23 +0900

    parse in progress, repl refactoring
    	modified:   data/fol.syntax
    	modified:   notebook/memo.ipynb
    	modified:   parser.jl
    	modified:   repl.jl

--- Resolution/repl.jl
+++ Resolution/repl.jl
@@ -10,7 +10,65 @@ function help()
  println("--- end of help")
 end
 
+commandmap = Dict(
+ "help"=> :help,
+ "ls"  => :lsfn,
+ "core"=> :corefn,
+ "show"=> :showfn
+)
+
+function lsfn(line,vars)
+ if strip(line) == "ls"
+  run(`ls`)
+ else 
+  dir=strip(line[3:end])
+  run(`ls $dir`)
+ end
+end
+
+function corefn(line,vars)
+  eqix=search(line,'=')
+  cnf=strip(line[(eqix+1):end])
+  name=strip(line[5:(eqix-1)])
+  print("cnf=$cnf => $name")
+  core = readcore(cnf)
+  vars[name] = core
+end
+
+function showfn(line,vars)
+  name=strip(line[length("show")+1:end])
+  printcore(vars[name])
+end
+
 function repl()
+ print("> ")
+ vars = Dict()
+ for line in eachline()
+ try
+  if line == "end"; return
+  elseif line == ""; 
+  elseif line == "?"; help()
+  elseif line == "help"; help()
+  else
+   println(line)
+   cix = search(line, ' ')
+   if cix == 0
+     cmd = strip(line)
+   else
+     cmd = strip(line[1:cix])
+   end
+   if cmd != ""
+     eval(Expr(:call, commandmap[cmd], line, vars))
+   end
+  end
+  catch e
+   println(e)
+  end
+  print("> ")
+ end
+end
+
+function repl0()
  print("> ")
  vars = Dict()
  for line in eachline()
