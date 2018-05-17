b35cee2b4f6d363b65ea436450fe5829e41fdd9b
sazare <song.of.sand@gmail.com>
2018-05-18 07:26:40 +0900

    add serial001.cnf for examining the mgu morphism add some grep pattern for data/*.cnf in README
    	modified:   README
    	modified:   data/rundata.jl
    	new file:   data/serial001.cnf

--- Prover/data/rundata.jl
+++ Prover/data/rundata.jl
@@ -68,3 +68,6 @@ rto1,cto1=simpleprover("data/to1.cnf",5,2)
 rto2,cto2=simpleprover("data/to2.cnf",5,2)
 rto3,cto3=simpleprover("data/to3.cnf",5,2)
 rto4,cto4=simpleprover("data/to4.cnf",5,2)
+
+rserial001,cserial001=simpleprover("data/serial001.cnf",5,2)
+
