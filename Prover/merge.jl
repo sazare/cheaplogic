8c1abb21b9ead5c31e556582cb8a05947e5c5771
sazare <song.of.sand@gmail.com>
2018-08-05 07:07:21 +0900

    merge.jl comes back. for graph-proving in future

---
+++ Prover/merge.jl
@@ -0,0 +1,40 @@
+## merge substitutions
+
+# this will be used in proving with graph
+# not yet implemented
+
+"""
+this merge may be a unfiy
+"""
+function merge(vars::Vlist, t1::Symbol, t2::Symbol)
+ if t1==t2; return t1 end
+ if isvar(t1,vars) && isvar(t2,vars)
+   return t2
+ elseif isvar(t1,vars)
+   return t2
+ elseif isvar(t2,vars)
+   return t1
+ else
+   throw(ICMP(t1,t2,:merge))
+ end
+end
+
+"""
+merge unifys 2 to 1 for substituions
+"""
+function merge(vars::Vlist, sub1::Tlist, sub2::Tlist)
+ if length(sub1) != length(sub2); throw(ICMP(sub1,sub2,:merge)) end
+ s1=apply(vars,sub1,sub2) # is s1 == sub1??
+ ss=apply(vars,sub2,s1)
+ s2=apply(vars,sub1,ss)
+ narg=[]
+ for i in 1:length(sub1)
+   mi = merge(vars, ss[i], s2[i])
+   if ndims(sub1) == 1
+     narg = vcat(narg,[mi])
+   else
+     narg = hcat(narg,[mi])
+   end
+ end 
+ narg
+end
---
+++ Prover/testmerge.jl
@@ -0,0 +1,40 @@
+include("merge.jl")
+
+using Base.Test
+
+@testset "merge 2 subst" begin
+# @test_throws Comparative merge([],:x,:a) 
+ @test merge([],:x,:x) == :x
+ @test merge([:x],:x,:x) == :x
+ @test merge([:x],:x,:a) == :a
+end
+
+@testset "apply subst" begin
+ @test apply([],:(P(x)),[]) == :(P(x))
+ @test apply([:x],:x,[:a]) == :a
+ @test apply([:x],:(x),[:a]) == :(a)
+ @test apply([:x],:(P(x)),[:a]) == :(P(a))
+ @test apply([:x,:y],:(P(x,f(y))),[:a,:b]) == :(P(a,f(b)))
+
+ @test_skip apply([:x,:y],:(P(x,y)),[:a,:x]) == :(P(a,a))
+ @test_skip apply([:x,:y],:(P(x,y)),[:y,:a]) == :(P(a,a))
+
+end
+
+@testset "merge arged" begin
+ @test merge([:x,:y],[:x,:y],[:x,:y]) == [:x,:y]
+ @test merge([:x,:y],[:x,:y],[:a,:b]) == [:a,:b]
+ @test merge([:x,:y],[:x,:a],[:x,:y]) == [:x,:a]
+ @test merge([:x,:y],[:x,:a],[:b,:a]) == [:b,:a]
+ @test merge([:x,:y],[:b,:y],[:x,:a]) == [:b,:a]
+ @test merge([:x,:y],[:a,:y],[:x,:y]) == [:a,:y]
+
+ @test merge([:x,:y],[:x,:x],[:a,:x]) == [:a,:a]
+ @test merge([:x,:y],[:x,:y],[:a,:x]) == [:a,:a]
+ @test merge([:x,:y],[:a,:y],[:y,:x]) == [:a,:a]
+ @test merge([:x,:y],[:a,:y],[:a,:x]) == [:a,:a]
+
+# @test_throws Comparative merge([:x,:y],[:a,:y],[:b,:y])
+# @test_throws Comparative merge([:x,:y],[:a,:y,:z],[:b,:y])
+
+end
