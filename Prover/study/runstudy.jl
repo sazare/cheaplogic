159f4c486d12fd7fd5e697f3ab80d8d09f2b023f
sazare <song.of.sand@gmail.com>
2018-12-16 17:35:05 +0900

    added samples for what is resolution refutation. 	new file:   study/runstudy.jl 	new file:   study/study001.cnf 	new file:   study/study001n.cnf 	new file:   study/study001n2.cnf 	new file:   study/study001n3.cnf 	new file:   study/study002.cnf 	new file:   study/study003.cnf 	new file:   study/study004.cnf 	new file:   study/study005.cnf

---
+++ Prover/study/runstudy.jl
@@ -0,0 +1,15 @@
+# infinit
+rstudy001,cstudy001=simpleprover("study/study001.cnf",12,3)
+rstudy001n,cstudy001n=simpleprover("study/study001n.cnf",12,3)
+rstudy001n2,cstudy001n2=simpleprover("study/study001n2.cnf",12,3)
+rstudy001n3,cstudy001n3=simpleprover("study/study001n3.cnf",12,3)
+
+# finite
+rstudy002,cstudy002=simpleprover("study/study002.cnf",4,3)
+rstudy003,cstudy003=simpleprover("study/study003.cnf",4,3)
+
+#
+rstudy004,cstudy004=simpleprover("study/study004.cnf",10,3)
+rstudy005,cstudy005=simpleprover("study/study005.cnf",3,3)
+#rstudy005,cstudy005=simpleprover("study/study005.cnf",20,15)
+
