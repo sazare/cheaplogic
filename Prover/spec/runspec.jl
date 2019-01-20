ae95da1875706dc4df050ecae0d38416ba774a3d
sazare <song.of.sand@gmail.com>
2019-01-20 19:57:12 +0900

    complete add.spec
    	modified:   spec/add.spec
    	modified:   spec/runspec.jl

--- Prover/spec/runspec.jl
+++ Prover/spec/runspec.jl
@@ -3,5 +3,7 @@
 df,cf,bf,eisf,insf,onsf=lps("spec/fact.jl", "spec/fact.spec", :fact, 3,3)
 db,cb,bb,eisb,insb,onsb=lps("spec/fibo.jl", "spec/fibo.spec", :fibo, 4,4)
 d1,c1,b1,eis1,ins1,ons1=lps("spec/id.jl", "spec/id.spec", :id, 3,3)
-da,ca,ba,eisa,insa,onsa=lps("spec/add.jl", "spec/add.spec", :add, 5,5)
+da,ca,ba,eisa,insa,onsa=lps("spec/add.jl", "spec/add.spec", :add, 4,4)
+
+println("all specs ran")
 
