0cb74a8ab5e1aae0b4a31cfad79e5b10d2743792
sazare <song.of.sand@gmail.com>
2019-03-21 18:28:06 +0900

    added a axioms k003.cnf dont work correctly
    	new file:   kpdata/kp003.cnf
    	modified:   kpdata/runkpdata.jl
    	modified:   test_kpbase.jl

--- Prover/kpdata/runkpdata.jl
+++ Prover/kpdata/runkpdata.jl
@@ -1,3 +1,4 @@
  rk001,ck001=ksimpleprover("kpdata/kp001.cnf",3,5)
  rk002,ck002=ksimpleprover("kpdata/kp002.cnf",2,2)
+ rk003,ck003=ksimpleprover("kpdata/kp003.cnf",5,5)
  rkj001,ckj001=ksimpleprover("kpdata/kpjp001.cnf",3,5)
