39009108907eaa510d4e4344296ee6ebad5ae227
sazare <song.of.sand@gmail.com>
2019-03-30 10:54:19 +0900

    added cnfs and update runkpdata.jl added a notebook
    kp005,kp006 are satle difference of Japanese joshi or
    variables OUTPUT or not.
    
    AnExampleOfAnalysis was just evaluated not changed.
    
    	modified:   kpdata/kp003.cnf
    	modified:   kpdata/kp004.cnf
    	new file:   kpdata/kp005.cnf
    	new file:   kpdata/kp006.cnf
    	modified:   kpdata/runkpdata.jl
    	modified:   note/AnExampleOfAnalysis.ipynb

--- Prover/kpdata/runkpdata.jl
+++ Prover/kpdata/runkpdata.jl
@@ -2,5 +2,8 @@
  rk002,ck002=kpsimpleprover("kpdata/kp002.cnf",2,2)
  rk003,ck003=kpsimpleprover("kpdata/kp003.cnf",5,5)
  rk004,ck004=kpsimpleprover("kpdata/kp004.cnf",5,5)
+ rk005,ck005=kpsimpleprover("kpdata/kp005.cnf",5,5)
+ rk006,ck006=kpsimpleprover("kpdata/kp006.cnf",5,5)
+
  rkj001,ckj001=kpsimpleprover("kpdata/kpjp001.cnf",3,5)
  rkj002,ckj002=kpsimpleprover("kpdata/kpjp002.cnf",3,5)
