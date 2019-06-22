5ec77a5a6fc3b1576eabf23f959474189db3074b
sazare <song.of.sand@gmail.com>
2019-06-23 08:27:58 +0900

    load_viewprover.jl works to ealuategoal.
    readcore vdata/vevlogic.cnf
    select C1 to valid
    select C2 to contradiction
    remove unused code from viewlogic.jl
    
    data/strcon.jl is a test cnf for string and char constant
    
    	modified:   coreprint.jl
    	new file:   data/strcon.cnf
    	modified:   dvcreso.jl
    	modified:   load_viewprover.jl
    	modified:   vdata/vl003.cnf
    	modified:   vlogic/vhtmls.jl
    	modified:   vlogic/viewlogic.jl
    	new file:   vlogic/viewmemo.txt
    	modified:   vlogic/viewreso.jl

--- Prover/load_viewprover.jl
+++ Prover/load_viewprover.jl
@@ -3,9 +3,9 @@ include("load_cheaplogic.jl")
 
 include("vlogic/viewreso.jl")
 include("vlogic/vhtmls.jl")
-include("vlogic/viewlogic.jl")
 
 # factify.jl is for first genie based system
 #include("vlogic/factify.jl")
 #include("vlogic/vlogic.jl")
 
+include("vlogic/viewlogic.jl")
