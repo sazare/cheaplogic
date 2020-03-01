ff19293330b0ee91d46ee2f97e7e3780119056f9
sazare <song.of.sand@gmail.com>
2020-03-01 10:53:34 +0900

    modified:   learn-lisp.lisp 	new file:   load-test.lisp 	new file:   ../../docs/about_subst.txt 	modified:   ../../docs/about_unify2.txt

--- Prover/bterm/lisp/learn-lisp.lisp
+++ Prover/bterm/lisp/learn-lisp.lisp
@@ -1,5 +1,5 @@
 ;; Practical Commonlisp CD DB
-(load "test.lisp")
+(load "load-test.lisp")
 
 (defvar *vvv* nil)
 (defun add-me (me) (push me *vvv*))
@@ -22,5 +22,12 @@
   (defparameter *plist* ())
   (setf (getf *plist* :a) 123)
   (expect-equal "get on a property list" 123 (getf *plist* :a))
+)
+
+;;
+(test-set "substitute"
+  (defvar *lll* '(a b c))
+  (expect-equal "atom to atom" '(a 2 c) (substitute 2 'b *lll*))
+  (expect-equal "atom to atom" '(a (f x) c) (substitute '(f x) 'b *lll*))
 )
 
---
+++ Prover/bterm/lisp/load-test.lisp
@@ -0,0 +1,13 @@
+(load "test.lisp")
+(terpri t)
+(terpri t)
+(terpri t)
+(terpri t)
+(terpri t)
+(format t "HERE WE BO~%")
+
+
+(load "test.lisp")
+(format t "~%... end of loading test utility~%")
+
+
