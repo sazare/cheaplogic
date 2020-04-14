75ba97d04560e95d8ea9eb9543594d43344e1b97
sazare <song.of.sand@gmail.com>
2020-04-14 19:58:31 +0900

    varsでp代入でunifyでは*でresolventは@でということを考えているメモ disagreeの詳細は闇の中
    	new file:   20200411unify.txt
    	modified:   20200412unify.txt
    	new file:   20200414unify.txt

--- Prover/bterm/lisp/learn-lisp.lisp
+++ Prover/bterm/lisp/learn-lisp.lisp
@@ -48,3 +48,45 @@
   (expect-equal "into subexp by subst" '((f (g z)) (g a)) (subst '(g z) 'x (subst 'a 'y *fff*)))
 )
 
+;; continuation
+;; first just loop on expr
+; (loop for x in x1 collect x)
+; (loop for a in x1 for b in x2 do (format t "~a:~a~%" a b))
+; (loop for a in x1 for b in x2 collect (cons a b))
+; (loop for s1 in '((a)(b)(c)) append s1 )
+
+(test-set "double loop"
+  (defvar x1 '(1 2 3))
+  (defvar x2 '(a b c))
+  (expect-equal "double loop" '((1 . a)(2 . b)(3 . c)) 
+    (loop for a in x1 for b in x2 collect (cons a b)))
+)
+
+(defun traverse2 (e1 e2)
+  (cond 
+    ((or (atom e1)(atom e2))(cons e1 e2))
+    (t (loop for se1 in e1 for se2 in e2 collect (traverse2 se1 se2)))
+  )
+)
+
+(defun traversediff (e1 e2)
+  (cond 
+;    ((equal e1 e2) ())
+    ((or (atom e1)(atom e2))(list (cons e1 e2)))
+    (t (loop for se1 in e1 for se2 in e2 unless (equal se1 se2) append (traversediff se1 se2)))
+  )
+)
+
+(test-set "traverse2"
+  (expect-equal "traverse2 lists" '((x . a)(y . z)) (traverse2 '(x y) '(a z)))
+  (expect-equal "traverse2 lists have same" '((x . a)(b . b)(y . z)) (traverse2 '(x b y) '(a b z)))
+)
+
+(test-set "traversediff"
+  (expect-equal "traversediff same" '() (traversediff '(x y) '(x y)))
+  (expect-equal "traversediff same depth" '() (traversediff '((f x) y) '((f x) y)))
+  (expect-equal "traversediff lists" '((x . a)(y . z)) (traversediff '(x y) '(a z)))
+  (expect-equal "traversediff lists have same" '((x . a)(y . z)) (traversediff '(x b y) '(a b z)))
+  (expect-equal "traversediff depth" '((x . a)(y . z)) (traversediff '(x (b y)) '(a (b z))))
+)
+
