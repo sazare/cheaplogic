4a0ecb89e8bd18ac8457a51ec30d05ac9e073147
sazare <song.of.sand@gmail.com>
2020-11-21 07:46:13 +0900

    renumbered the name of a2,a3   because previous names made conflict of cid a1 with a2 and a3
    	modified:   ../../docs/devlog
    	modified:   play-rubbish-prover.lisp

--- lisp/rubbish/play-rubbish-prover.lisp
+++ lisp/rubbish/play-rubbish-prover.lisp
@@ -7,11 +7,9 @@
 (defparameter a1 (readskqc "((1 () (+ A a)(+ B a)(+ C a)(+ D a)(+ E a))(2 () (- A a)) (3 () (- B a))(4 ()(- C a))(5 ()(- D a))(6 () (- E a)))"))
 
 (defparameter r7 (resolve-id 'L1-1 'L2-1))
-
 (defparameter r8 (resolve-id 'L7-1 'L3-1))
 (defparameter r9 (resolve-id 'L8-1 'L4-1))
 (defparameter r10 (resolve-id 'L9-1 'L5-1))
-
 (defparameter r11 (resolve-id 'L10-1 'L6-1))
 (defparameter r12 (resolve-id 'L8-2 'L5-1))
 ; to ito-rubbish-prover.lisp
@@ -25,7 +23,6 @@
 ;((+ C A) (+ D A) (+ E A))
 ;; occasionaly these are same, but generally different.
 ;; use variables, it becomes clear
-
 
 (resolve-id 'L3-1 'L1-2)
 ;C13
@@ -48,45 +45,48 @@
 
 ;;;;; with vars1
 
-(defparameter a2 (readskqc "((11 (x y z w u) (+ A x y)(+ B y z)(+ C z w)(+ D w u)(+ E u x))(12 () (- A a b)) (13 () (- B b c))(14 ()(- C c d))(15 ()(- D d e))(16 (x) (- E x x)))"))
+(defparameter a2 (readskqc "((21 (x y z w u) (+ A x y)(+ B y z)(+ C z w)(+ D w u)(+ E u x))(22 () (- A a b)) (23 () (- B b c))(24 ()(- C c d))(25 ()(- D d e))(26 (x) (- E x x)))"))
 
 (dump-clauses a2)
-;C11=(PROOF (RESOLUTION NIL NIL (L10-1 L6-1)) VARS
-;     (X.428 Y.429 Z.430 W.431 U.432) NAME 11)
-; L11-1 (+ A X.428 Y.429) = (OLID L11-1 PLID NIL CID C11)
-; L11-2 (+ B Y.429 Z.430) = (OLID L11-2 PLID NIL CID C11)
-; L11-3 (+ C Z.430 W.431) = (OLID L11-3 PLID NIL CID C11)
-; L11-4 (+ D W.431 U.432) = (OLID L11-4 PLID NIL CID C11)
-; L11-5 (+ E U.432 X.428) = (OLID L11-5 PLID NIL CID C11)
-;C12=(PROOF (RESOLUTION NIL NIL (L8-2 L5-1)) VARS NIL NAME 12)
-; L12-1 (- A A B) = (OLID L12-1 PLID NIL CID C12)
-;C13=(PROOF (RESOLUTION NIL NIL (L3-1 L1-2)) VARS NIL NAME 13)
-; L13-1 (- B B C) = (OLID L13-1 PLID NIL CID C13)
-;C14=(PROOF (RESOLUTION NIL NIL (L5-1 L13-3)) VARS NIL NAME 14)
-; L14-1 (- C C D) = (OLID L14-1 PLID NIL CID C14)
-;C15=(VARS NIL NAME 15)
-; L15-1 (- D D E) = (OLID L15-1 PLID NIL CID C15)
-;C16=(VARS (X.433) NAME 16)
-; L16-1 (- E X.433 X.433) = (OLID L16-1 PLID NIL CID C16)
-;NIL
-;
 
-(defparameter a3 (readskqc "((21 (x y z w u) (+ A x y)(+ B y z)(+ C z w)) (22 (w u x) (- B w u)(+ E u x)) (23 () (- A a b)) (24 (x y) (- C x y) (+ D  x y))(25 ()(- D d n))(26 ()(- E d e))(27 (x) (- E x x)))"))
 
-(resolve-id 'L21-2 'L22-1)
-;C28
-;* (dump-clause 'c28)
-;C28=(PROOF
-;     (RESOLUTION (X.434 Y.435 Z.436 W.437 U.438 W.439 U.440 X.441)
-;      (X.445 W.450 U.451 W.448 U.449 W.450 U.451 X.452) (L21-2 L22-1))
-;     VARS (X.445 W.448 U.449 W.450 U.451 X.452) NAME RESOLVENT)
-; L28-1 (+ A X.445 W.450) = (OLID L21-1 PLID L21-1 CID C28)
-; L28-2 (+ C U.451 W.448) = (OLID L21-3 PLID L21-3 CID C28)
-; L28-3 (+ E U.451 X.452) = (OLID L22-2 PLID L22-2 CID C28)
+(defparameter a3 (readskqc "((41 (x y z w u) (+ A x y)(+ B y z)(+ C z w)) (42 (w u x) (- B w u)(+ E u x)) (43 () (- A a b)) (44 (x y) (- C x y) (+ D  x y))(45 ()(- D d n))(46 ()(- E d e))(47 (x) (- E x x)))"))
+
+(dump-clauses a3)
+;C41=(VARS (X.429 Y.430 Z.431 W.432 U.433) NAME 41)
+; L41-1 (+ A X.429 Y.430) = (OLID L41-1 PLID NIL CID C41)
+; L41-2 (+ B Y.430 Z.431) = (OLID L41-2 PLID NIL CID C41)
+; L41-3 (+ C Z.431 W.432) = (OLID L41-3 PLID NIL CID C41)
+;C42=(VARS (W.434 U.435 X.436) NAME 42)
+; L42-1 (- B W.434 U.435) = (OLID L42-1 PLID NIL CID C42)
+; L42-2 (+ E U.435 X.436) = (OLID L42-2 PLID NIL CID C42)
+;C43=(VARS NIL NAME 43)
+; L43-1 (- A A B) = (OLID L43-1 PLID NIL CID C43)
+;C44=(VARS (X.437 Y.438) NAME 44)
+; L44-1 (- C X.437 Y.438) = (OLID L44-1 PLID NIL CID C44)
+; L44-2 (+ D X.437 Y.438) = (OLID L44-2 PLID NIL CID C44)
+;C45=(VARS NIL NAME 45)
+; L45-1 (- D D N) = (OLID L45-1 PLID NIL CID C45)
+;C46=(VARS NIL NAME 46)
+; L46-1 (- E D E) = (OLID L46-1 PLID NIL CID C46)
+;C47=(VARS (X.439) NAME 47)
+; L47-1 (- E X.439 X.439) = (OLID L47-1 PLID NIL CID C47)
 ;NIL
-(resolve-id 'L28-1 'L23-1)
-;C29
-;* (dump-clause 'c29)
+
+(defparameter r48 (resolve-id 'L41-2 'L42-1))
+(dump-clause r48)
+;C48=(PROOF
+;     (RESOLUTION (X.429 Y.430 Z.431 W.432 U.433 W.434 U.435 X.436)
+;      (X.440 W.445 U.446 W.443 U.444 W.445 U.446 X.447) (L41-2 L42-1))
+;     VARS (X.440 W.443 U.444 W.445 U.446 X.447) NAME RESOLVENT)
+; L48-1 (+ A X.440 W.445) = (OLID L41-1 PLID L41-1 CID C48)
+; L48-2 (+ C U.446 W.443) = (OLID L41-3 PLID L41-3 CID C48)
+; L48-3 (+ E U.446 X.447) = (OLID L42-2 PLID L42-2 CID C48)
+;NIL
+
+
+(defparameter r49 (resolve-id 'L48-1 'L43-1))
+(dump-clause r49)
 ;C29=(PROOF
 ;     (RESOLUTION (X.445 W.448 U.449 W.450 U.451 X.452)
 ;      (A W.454 U.455 B U.457 X.458) (L28-1 L23-1))
@@ -94,31 +94,20 @@
 ; L29-1 (+ C U.457 W.454) = (OLID L21-3 PLID L28-2 CID C29)
 ; L29-2 (+ E U.457 X.458) = (OLID L22-2 PLID L28-3 CID C29)
 ;NIL
-(resolve-id 'L29-1 'L24-1)
-;C30
-;* (dump-clause 'c30)
-;C30=(PROOF
-;     (RESOLUTION (W.454 U.455 U.457 X.458 X.442 Y.443)
-;      (Y.464 U.460 X.463 X.462 X.463 Y.464) (L29-1 L24-1))
-;     VARS (U.460 X.462 X.463 Y.464) NAME RESOLVENT)
-; L30-1 (+ E X.463 X.462) = (OLID L22-2 PLID L29-2 CID C30)
-; L30-2 (+ D X.463 Y.464) = (OLID L24-2 PLID L24-2 CID C30)
-;NIL
-(resolve-id 'l30-1 'l26-1)
-;C31
-;* (dump-clause 'c31)
-;C31=(PROOF
-;     (RESOLUTION (U.460 X.462 X.463 Y.464) (U.465 E D Y.468) (L30-1 L26-1))
-;     VARS (U.465 Y.468) NAME RESOLVENT)
-; L31-1 (+ D D Y.468) = (OLID L24-2 PLID L30-2 CID C31)
-;NIL
-(resolve-id 'l31-1 'l25-1)
-;C32
 
-(defparameter r3 '(c28 c29 c30 c31 c32))
+(defparameter r50 (resolve-id 'L49-1 'L44-1))
+(dump-clause r50)
+
+(defparameter r51 (resolve-id 'l50-1 'l46-1))
+(dump-clause r51)
+
+(defparameter r52 (resolve-id 'l51-1 'l45-1))
+(dump-clause r52)
+
+(defparameter r3s '(c48 c49 c50 c51 c52))
 
 
-(dump-clauses (append a3 r3))
-(ccode 'c28)
-(canonic 'c28)
+(dump-clauses (append a3 r3s))
+(ccode 'c48)
+(canonic 'c48)
 
