73dce2dcd201adb445bc860a88b7fa09bf3051f5
sazare <song.of.sand@gmail.com>
2020-12-31 09:18:08 +0900

    add peval for partial evaluation add statistics for prover-gtrail's statistics 	modified:   ../../docs/devlog 	new file:   rubbish-peval.lisp 	new file:   rubbish-statistics.lisp

---
+++ lisp/rubbish/rubbish-peval.lisp
@@ -0,0 +1,22 @@
+;rubbish-peval.lisp
+;; peval = partial evaluation
+;; 
+;; peval eval ground term to a value, but if it causes an error,
+;; to the term itself.
+;; but don't eval (+ x (+ 1 2)) to (+ x 3) because
+;; (+ x 3) can't be unified with (+ x (+ y 2)) 
+
+(defun peval (e) ;; e must be a quoted S-exp as '(+ 1 www)
+  (let (v)
+    (setq v (ignore-errors (eval e)))
+    (if v v e)
+  )
+)
+
+(defmacro mpeval (e) ;; e may be a form as (+ 1 xxx) 
+  `(let (v)
+    (setq v (ignore-errors ,e))
+    (if v v ',e)
+   )
+)
+
---
+++ lisp/rubbish/rubbish-statistics.lisp
@@ -0,0 +1,25 @@
+;; rubbish-statistics.lisp
+; variables for statistics
+
+(defparameter *num-of-input-literals* 0)   ;
+(defparameter *num-of-trials* 0)           ; how many unifications do?
+(defparameter *num-of-proof-steps* 0)      ; how long proof is?
+
+(defparameter *num-of-contradictions* 0)   ; # of contradictions(iscontradiction over *clist*)
+(defparameter *num-of-literals* 0)         ; = (length *llist*)
+(defparameter *input-clauses ())           ; as the *clist* immediately after read kqc file
+(defparameter *input-literals* ())         ; all literals of input clauses
+(defparameter *num-of-resolvents* 0)       ; # of resolvents = (*clist* - input-clause)
+
+
+(defun reset-stat ()
+  (setq *num-of-input-literals* 0)   
+  (setq *num-of-trials* 0)
+  (setq *num-of-proof-steps* 0)
+
+  (setq *num-of-contradictions* 0)
+  (setq *num-of-literals* 0)
+  (setq *input-clauses ())
+  (setq *input-literals* ())
+  (setq *num-of-resolvents* 0)
+)
