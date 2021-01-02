9c3fcbbf864c7fa02d88008cdf5d2421d9a156e9
sazare <song.of.sand@gmail.com>
2021-01-02 09:56:21 +0900

    added *peval-active* for to activated peval.  when *peval-active* is T, peval works
    	modified:   ../../docs/devlog
    	modified:   rubbish-peval.lisp
    	modified:   rubbish-prover-gtrail.lisp

--- lisp/rubbish/rubbish-peval.lisp
+++ lisp/rubbish/rubbish-peval.lisp
@@ -6,17 +6,23 @@
 ;; but don't eval (+ x (+ 1 2)) to (+ x 3) because
 ;; (+ x 3) can't be unified with (+ x (+ y 2)) 
 
+(defparameter *peval-active* t)
+
 (defun peval (e) ;; e must be a quoted S-exp as '(+ 1 www)
-  (let (v)
-    (setq v (handler-case (progn (eval e)) (error () :myerror)))
-    (if (eq v :myerror) e v)
+  (and *peval-active*
+    (let (v)
+      (setq v (handler-case (progn (eval e)) (error () :myerror)))
+      (if (eq v :myerror) e v)
+    )
   )
 )
 
 (defmacro mpeval (e) ;; e may be a form as (+ 1 xxx) 
-  `(let (v)
-    (setq v (handler-case (progn ,e) (error () :myerror)))
-    (if (eq v :myerror) ',e v)
+  `(and *peval-active*
+    (let (v)
+      (setq v (handler-case (progn ,e) (error () :myerror)))
+      (if (eq v :myerror) ',e v)
+     )
    )
 )
 
