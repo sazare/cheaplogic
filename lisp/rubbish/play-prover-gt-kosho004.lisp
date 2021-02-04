66760aa0fea78bab41f3db6089533c99561d637e
sazare <song.of.sand@gmail.com>
2021-02-04 21:53:45 +0900

    add switch004.kqc extends switch002.kqc  added just an observation (+ Bat empty).
    	new file:   ../../docs/about_kosho.txt
    	modified:   ../../docs/devlog
    	new file:   kqc/kosho/switch004.kqc
    	new file:   play-prover-gt-kosho004.lisp

---
+++ lisp/rubbish/play-prover-gt-kosho004.lisp
@@ -0,0 +1,14 @@
+;; kosho detect
+
+(load "load-rubbish.lisp")
+
+;; play for prover
+
+(setq *max-clauses* 100)
+(setq *max-trials* 100)
+(setq *max-steps* 100)
+(setq *timeout-sec* 1)
+
+(play-prover-gtrail '(1 2 3) "kqc/kosho/switch004.kqc")
+
+
