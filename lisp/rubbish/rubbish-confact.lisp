52f97efab96dad94cbb1f40bf9cdd619e282155a
sazare <song.of.sand@gmail.com>
2021-04-26 19:40:12 +0900

    added onfalse-kb rename onfact-kb from reduce-kb added reduce-kb for onfact-kb and onfalse-kb
    	modified:   ../../docs/EOLO/20210425_20doors.md
    	modified:   ../../docs/devlog
    	modified:   kqc/3doors.txt
    	modified:   rubbish-confact.lisp

--- lisp/rubbish/rubbish-confact.lisp
+++ lisp/rubbish/rubbish-confact.lisp
@@ -6,26 +6,56 @@
   (setq *kb* (reverse *clist*))
 )
 
-;reduce for a clause(knowledge)
-(defun reduce-clause (fcid cid)
+
+;;; in the following, separete reduce for fact and falsehood
+;;; because this is an experiment. 
+;;; the two procedure should be in one for efficiency.
+
+; fact reduce for a clause(knowledge)
+(defun onfact-clause (fcid cid)
   (let* ((flid (car (bodyof fcid))) (oflsym (oppolsymof (lsymof flid))) r1)
     (loop for lid in (bodyof cid) 
        when (eq (lsymof lid) oflsym)
-       do 
-          (unless (equal :fail (setq r1 (resolve-id flid lid))) (return r1))
+       do (unless (equal :FAIL (setq r1 (resolve-id flid lid))) (return r1))
        finally (return cid)
     )
   )
 )
 
-; reduce for knowledge base
-(defun reduce-kb (fcid kb)
+; fact reduce for knowledge base
+(defun onfact-kb (fcid kb)
  ;; fcid is the cid of the answer
  ;; 
     (loop for cid in kb 
       unless (eq cid fcid)
-      collect (reduce-clause fcid cid)
+      collect (onfact-clause fcid cid)
     )
+)
+
+; falsehood reduce for clause
+(defun onfalse-clause (fcid cid)
+  (let* ((flid (car (bodyof fcid))) (flsym (lsymof flid)) r1)
+    (loop for lid in (bodyof cid) 
+       when (eq (lsymof lid) flsym)
+       do (unless (equal :FAIL (setq r1 (resolve-id flid lid))) (return nil))
+       finally (return cid)
+    )
+  )
+)
+
+; falsehood reduce for knowledge base
+(defun onfalse-kb (fcid kb)
+;; if same lsym, they don't resolve
+  (let (r1)
+    (loop for cid in kb 
+      when (setq r1 (onfalse-clause fcid cid))
+      collect r1
+    )
+  )
+)
+
+(defun reduce-kb (fcid kb)
+  (onfalse-kb fcid (onfact-kb fcid kb))  
 )
 
 ;; find fact in kb
