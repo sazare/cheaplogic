7843a7da62b6d0eaf570ff46487c35f7437ec877
sazare <song.of.sand@gmail.com>
2021-05-14 16:17:30 +0900

    reboot flashlight kqc/FL/flash001.{lisp,wff,kqc} see docs/20210514_flash.txt also
    	new file:   docs/20210514_flashlight.txt
    	modified:   docs/devlog
    	modified:   lisp/rubbish/kqc/3doors.txt
    	new file:   lisp/rubbish/kqc/FL/flash001.kqc
    	new file:   lisp/rubbish/kqc/FL/flash001.lisp
    	new file:   lisp/rubbish/kqc/FL/flash001.wff

---
+++ lisp/rubbish/kqc/FL/flash001.lisp
@@ -0,0 +1,24 @@
+; flash light with EOLO 
+
+(defparameter fwname "kqc/FL/flash001.wff")
+(defparameter fkname "kqc/FL/flash001.kqc")
+
+
+(load "load-rubbish.lisp")
+(load "rubbish-confact.lisp")
+
+(readekqc fkname)
+(init-kb)
+
+(defparameter fc1 (make-clause '(() (- FL light))))
+(defparameter fc2 (make-clause '(() (+ SW on))))
+(defparameter fc3 (make-clause '(() (+ BAT empty))))
+
+(defparameter kb1 (reduce-kb fc1 *kb*))
+(findfact kb1)
+
+(defparameter kb2 (reduce-kb fc2  kb1))
+(findfact kb2)
+
+;; this should be +Bat(empty) not fact but lemma
+
