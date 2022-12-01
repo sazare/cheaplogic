4b7b9ce863ef8942ccb3c521892cc9e07ac9a721
OMURA Shinichi <song.of.sand@gmail.com>
2022-12-02 07:12:51 +0900

    fix parameters' order in print-proof functions and a trial to use rubbish.conf
    	modified:   play-prover-gt-ml002.lisp
    	modified:   rubbish-proof.lisp
    	new file:   rubbish.conf

--- lisp/rubbish/play-prover-gt-ml002.lisp
+++ lisp/rubbish/play-prover-gt-ml002.lisp
@@ -1,4 +1,3 @@
-
 ;; ters for reduce on input
 
 ;(load "load-rubbish.lisp")
@@ -7,12 +6,14 @@
 
 ;; play for prover
 
+(load "rubbish.conf")
+
 (in-package :rubbish)
 
-(setq *max-clauses* 50)
-(setq *max-trials* 100)
-(setq *max-steps* 100)
-(setq *timeout-sec* 1)
+;;(setq *max-clauses* 50)
+;;(setq *max-trials* 100)
+;;(setq *max-steps* 100)
+;;(setq *timeout-sec* 1)
 
 (play-prover-gtrail '(1) "kqc/ml002.kqc")
 
