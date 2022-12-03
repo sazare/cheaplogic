dda3d5c1b3c4eeeb756e255adee75347332fd8ab
OMURA Shinichi <song.of.sand@gmail.com>
2022-12-03 10:12:34 +0900

    update play-prover-gt-ml002.lisp and make pre-rubbish.lisp for demonstrating setup sequence and web is a web UI trial 1 	modified:   play-prover-gt-ml002.lisp 	new file:   web/basic.css 	new file:   web/basic.html

--- lisp/rubbish/play-prover-gt-ml002.lisp
+++ lisp/rubbish/play-prover-gt-ml002.lisp
@@ -1,19 +1,7 @@
 ;; ters for reduce on input
 
-;(load "load-rubbish.lisp")
-;(require :rubbish)
-(require :gtrail)
-
-;; play for prover
-
-(load "rubbish.conf")
+(load "pre-rubbish.lisp")
 
 (in-package :rubbish)
-
-;;(setq *max-clauses* 50)
-;;(setq *max-trials* 100)
-;;(setq *max-steps* 100)
-;;(setq *timeout-sec* 1)
-
 (play-prover-gtrail '(1) "kqc/ml002.kqc")
 
