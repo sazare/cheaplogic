4ea1f5c2d51fad840748b10507b5411377941938
OMURA Shinichi <song.of.sand@gmail.com>
2025-11-22 11:46:08 +0900

    utable almost runs. add 2 kqc for it too. 	modified:   docs/exhaustive/exhp-memo.txt 	new file:   kqc/exhp/umat001.kqc 	new file:   kqc/exhp/umat010.kqc 	modified:   rubbish-umatrix.lisp

--- lisp/rubbish/rubbish-umatrix.lisp
+++ lisp/rubbish/rubbish-umatrix.lisp
@@ -1,5 +1,7 @@
 ;; unifier matrix of P for same signs and oppo signs
 
+(in-package :rubbish)
+
 ;; unify-atoms
 (defun unify-atoms (lid1 lid2)
   (let* ((vs (union (varsof (cidof lid1)) (varsof (cidof lid2))))
@@ -10,10 +12,29 @@
   )
 )
 
-(defun make-umatrix (llids rlids)
-  (loop for llid in llids collect
-    (loop for rlid in rlids collect
-      (unify-atoms llid rlid)
+(defun make-umatrix (clist)
+  (let (ppns ppn)
+    (setq ppns (make-ppnlist clist))
+    (loop for pred in (make-psymlist (make-lidlist clist)) 
+      collect
+      (let ()
+        (setq ppn (assoc pred  ppns))
+        (list pred 
+           (make-umatrix-p (nth 1 ppn)(nth 1 ppn))
+           (make-umatrix-p (nth 2 ppn)(nth 2 ppn))
+           (make-umatrix-p (nth 1 ppn)(nth 2 ppn)))
+      )
+    )
+  )
+)
+
+(defun make-umatrix-p (llids rlids)
+  (list 
+    (list llids rlids)
+    (loop for llid in llids collect
+      (loop for rlid in rlids collect
+        (unify-atoms llid rlid)
+      )
     )
   )
 )
@@ -26,22 +47,39 @@
         (push tm ts)
     ) 
     (if (null vs) 
-      nil
+      :∅
       (list (reverse vs)(reverse ts))
     )
   )
 )
 
-(defparameter ppnlist  (make-ppnlist *clist*))
-
-(defun make-umatrix-of (ppnlist pid)
-  (let (ppn)
-    (setq ppn (make-ppnlist pid ppnlist))
-    (values 
-      (umatrix (cadr ppn)(cadr ppn)) 
-      (umatrix (caddr ppn) (caddr ppn))
-      (umatrix (cadr ppn) (caddr ppn))
+(defun pum-row (ml)
+  (loop for am in  ml do
+    (if (eq :NO am)
+      (format t "|NO")
+      (format t "|~a" (strip-mgu am))
     )
+  finally 
+    (format t "|~%")
+  )
+)
+ 
+(defun pum-p (pred aum)
+  "ums = (pred (ll1 ll2) (m11 m12 ...)(m21 m22 ...)...(mk1 mk2 ...))"
+  (let ()
+    (format t "~a~%" pred)
+    (format t "~a~%" (car aum))
+    (loop for aml in (cadr aum) do
+      (pum-row aml)
+    ) 
   )
 )
 
+(defun pum (ums)
+  (let (pred)
+    (setq pred (nth 0 ums))
+    (pum-p pred (nth 1 ums))
+    (pum-p pred (nth 2 ums))
+    (pum-p pred (nth 3 ums))
+  )
+) 
